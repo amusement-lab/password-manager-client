e99a45793acab5930efb736012d5fc25b3bbab90
Dipadana Putu <dipadanadev@gmail.com>
2024-05-13 14:39:19 +0800

    feat: add note field in password

--- src/pages/passwords/addPassword.tsx
+++ src/pages/passwords/addPassword.tsx
@@ -117,6 +117,23 @@ export default function Add() {
                     </div>
                   </div>
                 </div>
+
+                <div className="sm:col-span-full">
+                  <label
+                    htmlFor="note"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Note
+                  </label>
+                  <div className="mt-2">
+                    <textarea
+                      name="note"
+                      id="note"
+                      placeholder="Write any note related to the password here"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
               </div>
             </div>
 
